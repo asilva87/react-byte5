40f12100c0ceef03c3c2bae98d87eb9d7d52748c
André Silva <dev01766@proton.me>
2023-11-27 08:22:22 +0100

    Change description

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ export default function Home(): JSX.Element {
     return (
         <div className="about-site">
             <p>
-                Here is another app for <span className="about-site__byte5">byte5</span>. This time, written in <strong>React</strong>. This time it's more complex than the previous one in Vue. It is similar to the task assigned to me during the interview.
+                Here is another app for <span className="about-site__byte5">byte5</span>. This time, written in <strong>React</strong>. It uses API calls and hooks. It is similar to the task assigned to me during the interview.
             </p>
             <p>
                 It has been published using <strong>AWS S3</strong>.
